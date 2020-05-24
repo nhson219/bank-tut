a4171ee7a1d10cfd4f22367c38c87bdf74c28c8b
sonnh <sonnh>
2020-05-24 14:58:01 +0700

    m

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -11,7 +11,7 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
-    CORS(app, resources={r"/": {"origins": "http://localhost:4000"}})
+    CORS(app, resources={r"/": {"origins": "/"}})
     app.config.from_object(config_by_name[config_name])
     # CORS(app, origins="*", allow_headers=[
     # "Content-Type", "Authorization", "Access-Control-Allow-Credentials"], supports_credentials=True)
